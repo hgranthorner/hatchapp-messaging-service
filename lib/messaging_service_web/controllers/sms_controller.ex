46705f2744b6286e8161f8fb710c95aef2f7f7b3
Grant Horner <granthorner@builtecho.com>
2025-07-04 09:37:05 -0400

    experiment: validate incoming json at the controller level

--- lib/messaging_service_web/controllers/sms_controller.ex
+++ lib/messaging_service_web/controllers/sms_controller.ex
@@ -2,19 +2,49 @@ defmodule MessagingServiceWeb.SmsController do
   use MessagingServiceWeb, :controller
   alias MessagingService.Messaging
 
+  action_fallback MessagingServiceWeb.FallbackController
+
   def incoming(conn, params) do
     # TODO: forward message to user
     # TODO: validate incoming message comes from provider
     # validate_incoming_message(conn, params)
-    Messaging.insert_text(params)
-    # send_message_to_user(params)
-    send_resp(conn, 200, "OK")
+
+    case validate_json(params) do
+      # NOTE(grant): This may be overkill
+      {:ok, _changeset} ->
+        Messaging.insert_text(params)
+        send_resp(conn, 200, "OK")
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
   end
 
   def outgoing(conn, params) do
     # TODO: forward message to provider
-    Messaging.insert_text(params)
-    send_resp(conn, 200, "OK")
+    case validate_json(params) do
+      {:ok, changeset} ->
+        Messaging.insert_text(params)
+        send_resp(conn, 200, "OK")
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  defp validate_json(params) do
+    types = %{to: :string, from: :string, type: :string}
+
+    changeset =
+      {%{}, types}
+      |> Ecto.Changeset.cast(params, Map.keys(types))
+      |> Ecto.Changeset.validate_required([:to, :from, :type])
+
+    if changeset.valid? do
+      {:ok, changeset}
+    else
+      {:error, changeset}
+    end
   end
 
   # def provider_send_message(message) do
